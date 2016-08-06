92939f91e5168fd93d749183327496639ce2ca45
kchapelier <kevin.chapelier@gmail.com>
2016-08-06 11:34:00 +0200

    rescope, refactor, cleanup, write doc

--- README.md
+++ README.md
@@ -1,5 +1,61 @@
-# dxt
+# decode-dxt
 
-Codec for the DXT texture formats (work in progress).
+Decoder for the DXT texture formats.
 
-Currently only able to read DXT1, DXT3 and DXT5 textures.
+## Features
+
+ * Supports Block Compression 1, 2 and 3 (DXT1-5)
+ * Works in node and in browsers with Typed Array [support](http://caniuse.com/#feat=typedarrays)
+
+## Example
+
+An example using decode-dxt with [parse-dds](https://www.npmjs.com/package/parse-dds) to decode the first mipmap texture of a DDS file using a DXT format.
+
+```js
+var decodeDXT = require('decode-dxt'),
+    parseDDS = require('parse-dds');
+
+// parse the dds
+var ddsBuffer = new Uint8Array(... DDS file ...),
+    ddsData = parseDDS(ddsBuffer);
+
+// get the first mipmap texture
+var image = ddsData.images[0],
+    imageWidth = image.shape[0],
+    imageHeight = image.shape[1],
+    imageDataView = new DataView(ddsBuffer.buffer, image.offset, image.length);
+
+// convert the DXT texture to an Uint8Array containing RGBA data
+var rgbaData = decodeDXT(imageDataView, imageWidth, imageHeight, ddsData.format);
+```
+
+## Public API
+
+### Method
+
+**decode(imageDataView, imageWidth, imageHeight[, format = 'dxt1'])**
+
+Decode a given DXT image to an Uint8Array containing the RGBA data of the image.
+
+ - *imageDataView :* A DataView pointing directly to the data of the DXT image.
+ - *imageWidth:* The width of the image.
+ - *imageHeight:* The height of the image.
+ - *format:* The format of the image (dxt1, dxt2, dxt3, dxt4 or dxt5), default to dxt1.
+
+### Format flags
+
+**decode.dxt1**
+
+**decode.dxt2**
+
+**decode.dxt3**
+
+**decode.dxt4**
+
+**decode.dxt5**
+
+## Changelog
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 "use strict";
 
-var bc1 = require('./lib/bc1'),
-    bc2 = require('./lib/bc2'),
-    bc3 = require('./lib/bc3');
+var decodeBC1 = require('./lib/bc1'),
+    decodeBC2 = require('./lib/bc2'),
+    decodeBC3 = require('./lib/bc3');
 
-//TODO check correctness of readDXT2 and readDXT4
+/**
+ * Decode a DXT image to RGBA data.
+ * @param {DataView} imageDataView A DataView pointing directly to the data of the DXT image
+ * @param {int} width Width of the image
+ * @param {int} height Height of the image
+ * @param {string} [format='dxt1'] Format of the image (dxt1, dxt2, dxt3, dxt4 or dxt5)
+ * @returns {Uint8Array} Decoded RGBA data
+ * @throws Will throw if the format is not recognized
+ */
+function decode (imageDataView, width, height, format) {
+    var result;
 
-module.exports = {
-    readDXT1: function (imageData, width, height) {
-        return bc1.read(imageData, width, height);
-    },
-    readDXT2: function (imageData, width, height) {
-        return bc2.read(imageData, width, height, true);
-    },
-    readDXT3: function (imageData, width, height) {
-        return bc2.read(imageData, width, height, false);
-    },
-    readDXT4: function (imageData, width, height) {
-        return bc3.read(imageData, width, height, true);
-    },
-    readDXT5: function (imageData, width, height) {
-        return bc3.read(imageData, width, height, false);
+    format = format ? format.toLowerCase() : 'dxt1';
+
+    if (format === decode.dxt1) {
+        result = decodeBC1(imageDataView, width, height);
+    } else if(format === decode.dxt2) {
+        result = decodeBC2(imageDataView, width, height, true);
+    } else if(format === decode.dxt3) {
+        result = decodeBC2(imageDataView, width, height, false);
+    } else if(format === decode.dxt4) {
+        result = decodeBC3(imageDataView, width, height, true);
+    } else if(format === decode.dxt5) {
+        result = decodeBC3(imageDataView, width, height, false);
+    } else {
+        throw new Error('Unknown DXT format : \'' + format + '\'');
     }
-};
+
+    return result;
+}
+
+decode.dxt1 = 'dxt1';
+decode.dxt2 = 'dxt2';
+decode.dxt3 = 'dxt3';
+decode.dxt4 = 'dxt4';
+decode.dxt5 = 'dxt5';
+
+module.exports = decode;
